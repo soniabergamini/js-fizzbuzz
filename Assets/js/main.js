2cd72aa862b519a52c62562aee345a3773b31b9f
soniabergamini <sounia96@outlook.it>
2023-04-17 17:16:12 +0200

    feat(main): add mouseover function

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,5 +1,5 @@
 // SOLUZIONE 1 con innerHTML
-// Ciclo for 0-100
+// Ciclo for 1-100
 for (let i = 0; i < 100; i++) {
 
     // Stamp crea numeri da 1 a 100
@@ -10,30 +10,30 @@ for (let i = 0; i < 100; i++) {
 
     // Se Multiplo di 3 e di 5
     if (stamp % 3 == 0 && stamp % 5 == 0) {
-        console.log("FizzBuzz");
-        boxContainer.innerHTML += `<div class="bgBlue dFlex justyCenter alignCenter">FizzBuzz</div>`;
+        // console.log("FizzBuzz");
+        boxContainer.innerHTML += `<div class="bgBlue dFlex justyCenter alignCenter cPointer">FizzBuzz</div>`;
 
     // Se Solo Multiplo di 3
     } else if (stamp % 3 == 0 && stamp % 5 != 0) {
-        console.log("Fizz");
-        boxContainer.innerHTML += `<div class="bgPink dFlex justyCenter alignCenter">Fizz</div>`;
+        // console.log("Fizz");
+        boxContainer.innerHTML += `<div class="bgPink dFlex justyCenter alignCenter cPointer">Fizz</div>`;
 
     // Se Solo Multiplo di 5
     } else if (stamp % 3 != 0 && stamp % 5 == 0) {
-        console.log("Buzz");
-        boxContainer.innerHTML += `<div class="bgRed dFlex justyCenter alignCenter">Buzz</div>`;
+        // console.log("Buzz");
+        boxContainer.innerHTML += `<div class="bgRed dFlex justyCenter alignCenter cPointer">Buzz</div>`;
 
     // Se NON Multiplo di 3 e 5
     } else {
-        console.log(stamp);
-        boxContainer.innerHTML += `<div class="bgGreen dFlex justyCenter alignCenter">${stamp}</div>`;
+        // console.log(stamp);
+        boxContainer.innerHTML += `<div class="bgGreen dFlex justyCenter alignCenter cPointer">${stamp}</div>`;
     }
 
     // console.log( "contatore vale: ", i, " - stamp vale: ", stamp );
 }
 
 // SOLUZIONE 2 con append
-// Ciclo for 100-200
+// Ciclo for 101-200
 for (let i = 0; i < 100; i++) {
 
     // Stamp crea numeri da 1 a 100
@@ -42,32 +42,64 @@ for (let i = 0; i < 100; i++) {
     // SVILUPPO LOGICA CONDIZIONALE:
     const boxContainer2 = document.querySelector("section.boxContainer2");
     const box = document.createElement('div');
-    box.classList.add('dFlex', 'justyCenter', 'alignCenter',);
+    box.classList.add('dFlex', 'justyCenter', 'alignCenter', 'cPointer');
 
     // Se Multiplo di 3 e di 5
     if (stamp % 3 == 0 && stamp % 5 == 0) {
-        console.log("FizzBuzz");
         box.className += " bgBlue";
         box.append("FizzBuzz");
         boxContainer2.append(box);
 
+        // Visualizza Numero in hover
+        box.addEventListener("mouseover", 
+            function mouseOver(){
+                box.innerText = stamp;
+            }
+        );
+        box.addEventListener("mouseout",
+            function mouseOut() {
+                box.innerText = ("FizzBuzz");
+            }
+        );
+        
         // Se Solo Multiplo di 3
     } else if (stamp % 3 == 0 && stamp % 5 != 0) {
-        console.log("Fizz");
         box.className += " bgPink";
         box.append("Fizz");
         boxContainer2.append(box);
 
+        // Visualizza Numero in hover
+        box.addEventListener("mouseover",
+            function mouseOver() {
+                box.innerText = stamp;
+            }
+        );
+        box.addEventListener("mouseout",
+            function mouseOut() {
+                box.innerText = ("Fizz");
+            }
+        );
+
         // Se Solo Multiplo di 5
     } else if (stamp % 3 != 0 && stamp % 5 == 0) {
-        console.log("Buzz");
         box.className += " bgRed";
         box.append("Buzz");
         boxContainer2.append(box);
 
+        // Visualizza Numero in hover
+        box.addEventListener("mouseover",
+            function mouseOver() {
+                box.innerText = stamp;
+            }
+        );
+        box.addEventListener("mouseout",
+            function mouseOut() {
+                box.innerText = ("Buzz");
+            }
+        );
+
         // Se NON Multiplo di 3 e 5
     } else {
-        console.log(stamp);
         box.className += " bgGreen";
         box.append(stamp);
         boxContainer2.append(box);
