2468defc959c06b4fc6eb63ac9fd9a8a6779d042
soniabergamini <sounia96@outlook.it>
2023-04-17 15:39:51 +0200

    feat(main): add box container content and style

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,19 +1,32 @@
 // Ciclo for 0-100
 for (let i = 0; i < 100; i++) {
 
-    // Stampa crea numeri da 1 a 100
+    // Stamp crea numeri da 1 a 100
     let stamp = i+1 
 
+    // SVILUPPO LOGICA CONDIZIONALE:
+    const boxContainer = document.getElementById("boxContainer");
+
+    // Se Multiplo di 3 e di 5
     if (stamp % 3 == 0 && stamp % 5 == 0) {
         console.log("FizzBuzz");
+        boxContainer.innerHTML += `<div class="bgBlue dFlex justyCenter alignCenter">${stamp}</div>`;
+
+    // Se Solo Multiplo di 3
     } else if (stamp % 3 == 0 && stamp % 5 != 0) {
         console.log("Fizz");
+        boxContainer.innerHTML += `<div class="bgPink dFlex justyCenter alignCenter">${stamp}</div>`;
+
+    // Se Solo Multiplo di 5
     } else if (stamp % 3 != 0 && stamp % 5 == 0) {
         console.log("Buzz");
+        boxContainer.innerHTML += `<div class="bgRed dFlex justyCenter alignCenter">${stamp}</div>`;
+
+    // Se NON Multiplo di 3 e 5
     } else {
         console.log(stamp);
+        boxContainer.innerHTML += `<div class="bgGreen dFlex justyCenter alignCenter">${stamp}</div>`;
     }
 
     // console.log( "contatore vale: ", i, " - stamp vale: ", stamp );
-
 }
