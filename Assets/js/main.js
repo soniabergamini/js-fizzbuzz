3271bb1bcaee08a8b1c823784e9fbe5438cd11d4
soniabergamini <sounia96@outlook.it>
2023-04-17 15:42:59 +0200

    perf(js): replace numbers with words

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -10,17 +10,17 @@ for (let i = 0; i < 100; i++) {
     // Se Multiplo di 3 e di 5
     if (stamp % 3 == 0 && stamp % 5 == 0) {
         console.log("FizzBuzz");
-        boxContainer.innerHTML += `<div class="bgBlue dFlex justyCenter alignCenter">${stamp}</div>`;
+        boxContainer.innerHTML += `<div class="bgBlue dFlex justyCenter alignCenter">FizzBuzz</div>`;
 
     // Se Solo Multiplo di 3
     } else if (stamp % 3 == 0 && stamp % 5 != 0) {
         console.log("Fizz");
-        boxContainer.innerHTML += `<div class="bgPink dFlex justyCenter alignCenter">${stamp}</div>`;
+        boxContainer.innerHTML += `<div class="bgPink dFlex justyCenter alignCenter">Fizz</div>`;
 
     // Se Solo Multiplo di 5
     } else if (stamp % 3 != 0 && stamp % 5 == 0) {
         console.log("Buzz");
-        boxContainer.innerHTML += `<div class="bgRed dFlex justyCenter alignCenter">${stamp}</div>`;
+        boxContainer.innerHTML += `<div class="bgRed dFlex justyCenter alignCenter">Buzz</div>`;
 
     // Se NON Multiplo di 3 e 5
     } else {
